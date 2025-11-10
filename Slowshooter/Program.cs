cf897d012c8df664ab85076b33543cfdf48a888d
Shockingnews <gagemckenzie70@gmail.com>
2025-11-10 09:27:37 -0400

    health pack boundaries

--- Slowshooter/Program.cs
+++ Slowshooter/Program.cs
@@ -45,7 +45,8 @@ namespace Slowshooter
         static (int, int) p1_min_max_y = (1, 3);
         static (int, int) p2_min_max_x = (1, 13);
         static (int, int) p2_min_max_y = (1, 3);
-
+        static (int, int) healthPack_x = (1, 13);
+        static (int, int) healthPack_y = (1, 3);
         // what turn is it? will be 0 after game is drawn the first time
         static int turn = -1;
 
